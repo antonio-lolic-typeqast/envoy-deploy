084d8411c089f360fbbf6fdfa16726a4099231e0
antonio-lolic-typeqast <antonio.lolic@typeqast.com>
2019-02-19 08:54:20 +0100

    Envoy configuration for Magento

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -63,10 +63,6 @@
 
 @servers(['web' => $server, 'localhost' => '127.0.0.1'])
 
-############################
-####  LARAVEL
-#########################
-
 @task('lara_init_env_file', ['on' => 'localhost'])
 	echo "Environment file set up $env $appPrefix";
 	#echo "/home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env  {{ $server }}:{{ $path }}";
@@ -78,18 +74,22 @@
 
 @task('lara_permission_setup', ['on' => 'web'])
 	echo "Permissions  setup";
+cd {{ $path }}
+    sudo chown -R www-data:www-data .
 	sudo find . -type d -exec chmod 755 {} \;
 	sudo find . -type f -exec chmod 644 {} \;
 	sudo chgrp -R www-data storage bootstrap/cache
 	sudo chmod -R ug+rwx storage bootstrap/cache
 	sudo chmod 777 -R public
-	#sudo chown -R ubuntu: public/build
-@endtaskt
+	#sudo chown -R ubuntu:www-data public/build
+@endtask
+
 @task('lara_install_dependencies', ['on' => 'web'])
 	echo "Install composer";
 	export COMPOSER_ALLOW_SUPERUSER=1
 	composer install --prefer-dist --no-dev --optimize-autoloader
-@endtaskt
+@endtask
+
 @task('lara_database_migrate', ['on' => 'web'])
 	# Migrate DB
 	#php artisan migrate --step
@@ -98,7 +98,8 @@
 		cp {{ $path }}/database/migrations/2018_03_09_140652_multi-server-event.php {{ $path }}/database/migrations/test
 		php artisan migrate --migrate=database/migrations/test
 	fi
-@endtaskt
+@endtask
+
 @task('lara_yarn_dependencies', ['on' => 'web'])
 	#NPM dependencies
 	yarn install
@@ -107,7 +108,7 @@
 	sudo gulp --production
 	#only shop , nad admin?
 	php artisan template:css
-@endtaskt
+@endtask
 
 @task('lara_after_install', ['on' => 'web'])
 
@@ -115,10 +116,12 @@
 	php artisan cache:clear
 	php artisan view:clear
 	php artisan route:clear
-	php artisan config:cache
+	php artisan config:clear
+	php artisan clear-compiled
 	php artisan optimize
+	php artisan config:cache
 	php artisan route:cache
-@endtaskt
+@endtask
 
 @task('lara_restart_services', ['on' => 'web'])
 	echo "Reload PHP-FPM and Nginx";
@@ -134,11 +137,6 @@
 	php artisan up
 @endtask
 
-############################
-### END LARAVEL
-#########################
-
-#########################################################################
 @story('lara')
 	start_deployment
 	lara_init_env_file
@@ -157,15 +155,12 @@
 	mage_init_env_file
 	mage_permission_setup
 	mage_install_dependencies
-	mage_database_migrate
 	mage_yarn_dependencies
 	mage_after_install
 	mage_restart_services
 	health_check
 	end_deployment
 @endstory
-
-#################################################################################
 
 @task('start_deployment', ['on' => 'web'])
 	echo "Starting deployment on s <$env> environment branh <$branch> app <$appPrefix>";
@@ -174,8 +169,11 @@
 	cd /var/www/html
 	#git clone https://uueqast:tpezzqast1zzzzz790@github.com/Typeqast/WeermanCentralApi.git
 	git clone {{ $repo }}
-	cd {{ $path }}
-@task
+
+	#cd {{ $path }}
+	#git checkout develop
+    #git pull origin develop
+@endtask
 
 @task('health_check')
 	@if ( ! empty($healthUrl) )
@@ -202,74 +200,74 @@
 	#ssh-add ~/.ssh/Weerman_prod_eu-west-1.pem
 	#ssh-add ~/.ssh/Weerman_qa_eu-west-1.pem
 @endtask
-##################################################3
-
-############################
-### MAGENTO
-#########################
 
 @task('mage_init_env_file', ['on' => 'localhost'])
 	echo "Environment file set up $env $appPrefix";
 	#echo "/home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env  {{ $server }}:{{ $path }}";
 	#aws s3 cp s3://wm-cf-templates/env_files/{{ $env }}/{{ $appPrefix }}/.env ./.env
 
-	echo "scp -r -i /home/antonio88/.ssh/Weerman_{{ $env }}_eu-west-1.pem /home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env {{ $server }}:{{ $path }}/";
-	scp -r -i /home/antonio88/.ssh/Weerman_{{ $env }}_eu-west-1.pem /home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env {{ $server }}:{{ $path }}/
+	echo "scp -r -i /home/antonio88/.ssh/Weerman_{{ $env }}_eu-west-1.pem /home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env {{ $server }}:{{ $path }}/app/etc";
+	scp -r -i /home/antonio88/.ssh/Weerman_{{ $env }}_eu-west-1.pem /home/antonio88/Desktop/env_files/{{ $env }}/{{ $appPrefix }}/.env {{ $server }}:{{ $path }}/app/etc
 @endtask
 
 @task('mage_permission_setup', ['on' => 'web'])
-	echo "Permissions  setup";
-	sudo find . -type d -exec chmod 755 {} \;
-	sudo find . -type f -exec chmod 644 {} \;
-	sudo chgrp -R www-data storage bootstrap/cache
-	sudo chmod -R ug+rwx storage bootstrap/cache
-	sudo chmod 777 -R public
-	@endtaskt
-	@task('lara_install_dependencies', ['on' => 'web'])
+    sudo chown -R www-data:www-data .
+
+	# Install user
+	#sudo adduser magento_user
+	#sudo passwd magento_user_password
+	#sudo usermod -a -G www-data magento_user
+
+    echo "Permissions  setup";
+	cd {{ $path }} && find var generated vendor pub/static pub/media app/etc -type f -exec chmod u+w {} + && find var generated vendor pub/static pub/media app/etc -type d -exec chmod u+w {} + && chmod u+x bin/magento
+
+@endtask
+
+@task('mage_install_dependencies', ['on' => 'web'])
 	echo "Install composer";
 	export COMPOSER_ALLOW_SUPERUSER=1
 	composer install --prefer-dist --no-dev --optimize-autoloader
-@endtaskt
+@endtask
 
-@task('mage_database_migrate', ['on' => 'web'])
-	# Migrate DB
-	#php artisan migrate --step
-	@endtaskt
-	@task('lara_yarn_dependencies', ['on' => 'web'])
+@task('mage_yarn_dependencies', ['on' => 'web'])
 	#NPM dependencies
 	yarn install
-	#Gulp
-	#sudo npm rebuild node-sass
-	sudo gulp --production
-@endtaskt
+	# Grunt
+	grunt:clean
+    grunt:exec
+    grunt:less
+@endtask
 
 @task('mage_after_install', ['on' => 'web'])
-	# Clear any previous cached views and optimize the application
-	php artisan cache:clear
-	php artisan view:clear
-	php artisan route:clear
-	php artisan config:cache
-	php artisan optimize
-	php artisan route:cache
-@endtaskt
+	echo "Set Deploy mode";
+	rm -rf var/cache/* generated/metadata/* generated/code/* var/view_preproccessed/* var/page_cache/* pub/static/*
+	php bin/magento deploy:mode:set production
+
+	echo "Clear any previous cached views and optimize the application";
+   # php bin/magento dev:source-theme:deploy en_US nl_NL
+    php bin/magento setup:static-content:deploy en_US nl_NL
+    php bin/magento setup:di:compile
+    php bin/magento maintenance:enable
+    php bin/magento setup:upgrade --keep-generated
+	php bin/magento setup:store-config:set --base-url="http://localhost:8080/"
+#php bin/magento setup:store-config:set --base-url="http://localhost:8080/"
+#php bin/magento setup:store-config:set --base-url-secure="https://localhost:8080/"
+    php bin/magento cache:flush
+    php bin/magento cache:clean
+	php bin/magento indexer:reindex
+	php bin/magento maintenance:disable
+@endtask
 
 @task('mage_restart_services', ['on' => 'web'])
 	echo "Reload PHP-FPM and Nginx";
-	sudo service php7.1-fpm reload
+	sudo service php7.2-fpm reload
 	sudo service nginx restart
 	sudo service nginx reload
 
 	echo "Supervisor restart processes";
 	sudo supervisorctl restart all
-	php artisan queue:restart
 
-	echo "Bring application up";
-	php artisan up
 @endtask
-
-############################
-### END MAGENTO
-#########################3
 
 {{--
 @finished
